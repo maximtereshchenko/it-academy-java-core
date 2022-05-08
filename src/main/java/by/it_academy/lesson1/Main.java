692b8340bd3ae153a266e5058d6e30df71240613
Xini1 <tereshchenko.xini@gmail.com>
2022-05-08 11:08:02 +0300

    lesson 1

---
+++ src/main/java/by/it_academy/lesson1/Main.java
@@ -0,0 +1,11 @@
+package by.it_academy.lesson1;
+
+/**
+ * @author Maxim Tereshchenko
+ */
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
