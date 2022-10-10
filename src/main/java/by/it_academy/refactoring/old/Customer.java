9e2b0a98116121329b7f82050f4564449740009e
Maxim Tereshchenko <maksimt@backbase.com>
2022-10-10 17:02:11 +0200

    bracers in IFs

--- src/main/java/by/it_academy/refactoring/old/Customer.java
+++ src/main/java/by/it_academy/refactoring/old/Customer.java
@@ -26,23 +26,26 @@ class Customer {
             switch (each.getMovie().getMovieType()) {
                 case REGULAR:
                     thisAmount += 2;
-                    if (each.getDaysRented() > 2)
+                    if (each.getDaysRented() > 2) {
                         thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
                     break;
                 case NEW_RELEASE:
                     thisAmount += each.getDaysRented() * 3;
                     break;
                 case CHILDREN:
                     thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
+                    if (each.getDaysRented() > 3) {
                         thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
                     break;
             }
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getMovieType() == Movie.MovieType.NEW_RELEASE) && each.getDaysRented() > 1)
+            if ((each.getMovie().getMovieType() == Movie.MovieType.NEW_RELEASE) && each.getDaysRented() > 1) {
                 frequentRenterPoints++;
+            }
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
